ec7c99145fc11f216e7b9bb54f1e89bd51919bad
egwilborn <egwilborn@gmail.com>
2023-06-29 20:17:46 -0500

    first commit

---
+++ models/flight.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema(
+  {
+    airline: { type: String, enum: ["Southwest", "American", "United"] },
+    airport: {
+      type: String,
+      enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+      default: "DEN",
+    },
+    flightNo: { type: Number, min: 10, max: 9999 },
+    departs: {
+      type: Date,
+      default: () => {
+        return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//note: default value doesn't seem to work??
+module.exports = mongoose.model("Flight", flightSchema);
